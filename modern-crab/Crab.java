1d4e43bad77eb9a1823deca929ffe07cf69d4185
Ravneet Singh <6291544@edu.vaniercollege.qc.ca>
2024-09-12 14:54:57 -0400

    Made lobster zombie infectious Put eating and dying sound effects and put background music

--- modern-crab/Crab.java
+++ modern-crab/Crab.java
@@ -15,12 +15,35 @@ public class Crab extends Actor
      */
     public void act()
     {
+        moveAndTurn();
+        eat();
+    }
+
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void moveAndTurn()
+    {
         move(4);
         if (Greenfoot.isKeyDown("left")) {
             turn(-3);
         }
         if (Greenfoot.isKeyDown("right")) {
             turn(3);
+        }
+    }
+
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void eat()
+    {
+        Actor worm = getOneIntersectingObject(Worm.class);
+        if (worm != null) {
+            World world = getWorld();
+            world.removeObject(worm);
+            Greenfoot.playSound("eating.wav");
+            Greenfoot.playSound("nightfall-future-bass-music-228100.wav");
         }
     }
 }
--- modern-crab/CrabWorld.java
+++ modern-crab/CrabWorld.java
@@ -17,5 +17,38 @@ public class CrabWorld extends World
     public CrabWorld()
     {
         super(560, 560, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab =  new  Crab();
+        addObject(crab, 382, 160);
+        Worm worm =  new  Worm();
+        addObject(worm, 109, 249);
+        Worm worm2 =  new  Worm();
+        addObject(worm2, 293, 261);
+        Worm worm3 =  new  Worm();
+        addObject(worm3, 341, 253);
+        Worm worm4 =  new  Worm();
+        addObject(worm4, 235, 356);
+        Worm worm5 =  new  Worm();
+        addObject(worm5, 109, 52);
+        Worm worm6 =  new  Worm();
+        addObject(worm6, 512, 59);
+        Worm worm7 =  new  Worm();
+        addObject(worm7, 108, 489);
+        Worm worm8 =  new  Worm();
+        addObject(worm8, 404, 444);
+        Worm worm9 =  new  Worm();
+        addObject(worm9, 516, 286);
+        Worm worm10 =  new  Worm();
+        addObject(worm10, 266, 66);
+        Lobster lobster =  new  Lobster();
+        addObject(lobster, 191, 170);
     }
 }
